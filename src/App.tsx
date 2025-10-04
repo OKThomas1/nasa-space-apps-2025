cef4e4cf1ae3aa25d05ba74b4057028e2519c49b
Zach Bourque <zachbourque01@gmail.com>
2025-10-04 14:33:18 -0700

    add time slider

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import "./App.css"
 import { MapView } from "./components/map-view"
+import { TimeToolbar } from "./time/TimeToolbar"
 import { ToolBar } from "./ToolBar"
 
 function App() {
     return (
         <div className="relative h-screen w-screen">
             <MapView />
-            <div className="absolute z-10 top-0">
+
+            {/* Bottom Bar */}
+            <div className="absolute flex gap-4 bottom-4 w-full max-h-32 h-full justify-center pointer-events-none [&>*]:pointer-events-auto">
                 <ToolBar />
+                <TimeToolbar />
             </div>
         </div>
     )
---
+++ src/time/TimeToolbar.tsx
@@ -0,0 +1,15 @@
+import FormLabel from "@mui/material/FormLabel"
+import Slider from "@mui/material/Slider"
+import { useTimeContext } from "./TimeContext"
+
+const TimeToolbar = () => {
+    const { time, setTime } = useTimeContext()
+    return (
+        <div className="flex flex-col h-full min-w-64 bg-gray-100 px-3 py-2 rounded-lg justify-center">
+            <FormLabel>Time</FormLabel>
+            <Slider valueLabelDisplay="auto" value={time} onChange={(_, v) => setTime(v)} />
+        </div>
+    )
+}
+
+export { TimeToolbar }
