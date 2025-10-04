ea34103f1a84b3908d2c46dfebd143820abb4117
Zach Bourque <zachbourque01@gmail.com>
2025-10-04 14:52:45 -0700

    basic legend

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import "./App.css"
 import { MapView } from "./components/map-view"
+import { Legend } from "./legend/Legend"
 import { TimeToolbar } from "./time/TimeToolbar"
 import { ToolBar } from "./ToolBar"
 
@@ -12,6 +13,11 @@ function App() {
             <div className="absolute flex gap-4 bottom-4 w-full max-h-32 h-full justify-center pointer-events-none [&>*]:pointer-events-auto">
                 <ToolBar />
                 <TimeToolbar />
+            </div>
+
+            {/* Right Bar */}
+            <div className="absolute flex flex-col justify-center right-4 bottom-0 h-full max-w-32 w-full pointer-events-none [&>*]:pointer-events-auto">
+                <Legend />
             </div>
         </div>
     )
